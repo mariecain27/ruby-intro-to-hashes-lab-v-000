6a6dcda9ad3fec6e0928df4605d7f1584a07f880
Marie Cain <mariecain27@gmail.com>
2018-06-09 01:51:16 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,13 @@ def monopoly_with_third_tier
 	monopoly = {:railroads => 
 	    {
 	     :pieces=> 4,
-	     :names=> {},
+	     :names=> 
+	     {	           
+        :reading_railroad=> {},
+        :pennsylvania_railroad=> {},
+        :b_and_o_railroad=> {},
+        :shortline=> {}
+        },
 	     :rent_in_dollars=> 
 	     {
 	        :one_piece_owned=>25,
@@ -35,7 +41,7 @@ def monopoly_with_third_tier
           :four_pieces_owned=>200
 	      }
 	    } 
-  }
+    }
 		
 end
 
